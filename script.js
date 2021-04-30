1d7766c3446aae3a473460707fb8db41946f958e
Margareta <margaretavida120@gmail.com>
2021-04-30 19:39:21 +0200

    nem lett megoldás hurrá

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 let gomb = document.getElementById("gomb");
 
+
+
 gomb.addEventListener("click", feltunes);
 
 function feltunes() {
   
   let body = document.getElementsByTagName("body")[0];
   let table = document.createElement("table");
-  let tthead = document.createElement("thead");
   let ttBody = document.createElement("tbody");
 
   for (let i = 0; i < 20; i++) {
    
-    let row = document.createElement("tr");
+    let col = document.createElement("tr");
 
     for (let j = 0; j < 2; j++) {
       
       let cell = document.createElement("td");
-
-
-
       let cellText = document.createTextNode(i+"!");
       cell.appendChild(cellText);
-      row.appendChild(cell);
+      col.appendChild(cell);
+      cell.style.width = "150px";
+    col.style.margin = "20px";
     }
 
-
-    ttBody.appendChild(row);
+    ttBody.appendChild(col);
   }
+
+  
 
     
   table.appendChild(ttBody);
-  table.appendChild(tthead);
-  tthead.appendChild(ttbody);
   body.appendChild(table);
-  table.setAttribute("border", "2");
+  table.setAttribute("border", "4");
 }
+
+
+
